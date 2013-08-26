21ff92147fe1f1bfa7a14bba635dbc2be2718e01
Jason Thomas <jason@lithiumfox.com>
2013-08-26 17:32:34 -0400

    Add exception handling

--- postmail/post.py
+++ postmail/post.py
@@ -17,14 +17,28 @@ def save_email(content):
         fp.write(content)
 
 
-content = sys.stdin.read()
+raw_email = sys.stdin.read()
 
-if config.save_email:
-    save_email(content)
+if config.save_email == True:
+    save_email(raw_email)
 
-email = {'body': content}
+#email = {'body': raw_email}
+email = {'bod1y': raw_email}
 
-r = requests.post(config.end_point, data=email)
-print r.text
-print r.headers
-print  (r.status_code == requests.codes.ok)
+try:
+    r = requests.post(config.end_point, data=email)
+
+# if there is a http connection or timeout issue, have postfix try again.
+except requests.ConnectionError as c:
+    print 'ConnectionError: %s' % c
+    sys.exit(75)
+except requests.Timeout as t:
+    print 'Timeout:m %s' % t
+    sys.exit(75)
+
+# if respose code is not equal to 201, print headers to log, bounce.
+if r.status_code != 201:
+    print r.text
+    print r.headers
+    save_email(raw_email)
+    sys.exit(78)
